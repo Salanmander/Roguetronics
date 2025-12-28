feab370cac7eb11bcf9da3941a2b43e8392c02f8
Salanmander <Alan.Kraut@gmail.com>
2025-12-27 22:29:50 -0800

    added conveyor test

--- Tests/test_all.gd
+++ Tests/test_all.gd
@@ -9,7 +9,7 @@ func before_all():
 	
 
 # Single widget on conveyor
-func test_passes():
+func test_conveyor_1():
 	factory = factory_packed.instantiate()
 	
 	add_child(factory)
@@ -27,6 +27,45 @@ func test_passes():
 	
 	assert_eq(floor.local_to_map(assembly1.position), Vector2i(3, 1))
 	
+	remove_child(factory)
+	factory.queue_free()
+	
+	# time to let cleanup happen
+	await wait_seconds(0.05)
+	
+
+# Test crane repeatedly pushing long line of widgets
+func test_crane_repeat():
+	factory = factory_packed.instantiate()
+	
+	add_child(factory)
+	
+	var floor: FactoryFloor = factory.get_node("FactoryLayer/FactoryFloor")
+	
+	
+	var track: Track = floor.make_track(Vector2i(3, 3))
+	track.drag_to(Vector2i(4, 3))
+	track.drag_to(Vector2i(5, 3))
+	
+	floor.make_dispenser(Vector2i(4, 3), 1)
+	var crane: Crane = floor.make_crane(Vector2i(4, 3), track)
+	
+	var program: Array[int] = [Consts.GRAB,
+							Consts.FORWARD, 
+							Consts.RELEASE,
+							Consts.BACKWARD,
+							Consts.GRAB,
+							Consts.BACKWARD,
+							Consts.RELEASE,
+							Consts.FORWARD]
+	crane.set_program(program)
+	
+	floor._on_run_pressed()
+	
+	await wait_seconds(25)
+	
+	assert(not factory.floor.crashed)
+
 	remove_child(factory)
 	factory.queue_free()
 	
